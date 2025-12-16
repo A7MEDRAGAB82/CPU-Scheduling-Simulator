869d6ef2f45bd0f5ae6f68805b12cd9b42ed6267
Ahmed Ragab <sci.AhmedRagab85728@alexu.edu.eg>
2025-12-16 22:00:18 +0200

    Implement user input parsing and vector storage

--- CPU-Scheduling-Simulator.cpp
+++ CPU-Scheduling-Simulator.cpp
@@ -1,6 +1,34 @@
 #include <iostream>
+#include <vector>
+#include "Process.h"
+
+using namespace std;
+
+
+
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+   vector<Process> processes ;
+
+   cout << "Please enter the number of processes: ";
+   int NumOfProcesses = 0;
+   cin >> NumOfProcesses;
+   for (int i = 0;i<NumOfProcesses;i++)
+   {
+       int pid, at, bt, prio;
+       cout << "Enter ProcessID, arrival time, burst time, and priority for the process : ";
+       cin >> pid >> at >> bt >> prio;
+        processes.push_back(Process(pid, at, bt, prio));
+   }
+
+   for (auto &i : processes)
+   {
+       cout << "Process ID: " << i.pid << ", Arrival Time: " << i.arrivalTime
+            << ", Burst Time: " << i.burstTime << ", Priority: " << i.priority << endl;
+   }
+
+
+    
 }
