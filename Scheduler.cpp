b8be3c0d8c7229ea7ae2831372437ebc61bdae10
Ahmed Ragab <sci.AhmedRagab85728@alexu.edu.eg>
2025-12-19 13:20:28 +0200

    Implement FCFS sorting and calculation logic

--- Scheduler.cpp
+++ Scheduler.cpp
@@ -2,6 +2,29 @@
 #include <algorithm>
 #include <iostream>
 
-void FCFS(vector<Process> processes) {
+void FCFS(vector<Process>& processes)
+{
+    sort(processes.begin(), processes.end(),
+        [](const Process& a, const Process& b) {
+            if (a.arrivalTime == b.arrivalTime)
+                return a.pid < b.pid;
+            return a.arrivalTime < b.arrivalTime;
+        });
 
+    int currentTime = 0;
+
+    for (auto& process : processes)
+    {
+        // CPU idle case
+        if (currentTime < process.arrivalTime)
+            currentTime = process.arrivalTime;
+
+        // Execute process
+        currentTime += process.burstTime;
+
+        // results
+        process.completionTime = currentTime;
+        process.turnaroundTime = process.completionTime - process.arrivalTime;
+        process.waitingTime = process.turnaroundTime - process.burstTime;
+    }
 }
